5c28f90bff792fd476631b7875a792ca5119137f
Koert van der Veer <koert@ondergetekende.nl>
2017-01-09 09:34:59 +0100

    Switch to truly lazy implementation by /u/nightcracker
    Still doesn't quite compete: http://i.imgur.com/aQHPwmn.png

--- lazy_sort/merge_sort.py
+++ lazy_sort/merge_sort.py
@@ -1,15 +1,29 @@
 import heapq
 
 
-def merge_sort(unsorted):
-    # Don't sort already-sorted list
-    if len(unsorted) <= 1:
-        yield from unsorted
-        return
+# def merge_sort(unsorted):
+#     # Don't sort already-sorted list
+#     if len(unsorted) <= 1:
+#         yield from unsorted
+#         return
 
-    midpoint = len(unsorted) // 2
+#     midpoint = len(unsorted) // 2
 
-    yield from heapq.merge(
-        merge_sort(unsorted[:midpoint]),
-        merge_sort(unsorted[midpoint:])
-    )
+#     yield from heapq.merge(
+#         merge_sort(unsorted[:midpoint]),
+#         merge_sort(unsorted[midpoint:])
+#     )
+
+def merge_sort(unsorted, start=None, end=None):
+    if start is None:
+        start = 0
+        end = len(unsorted)
+
+    midpoint = (start + end) // 2
+
+    if end - start == 1:
+        yield unsorted[start]
+    elif end - start > 1:
+        yield from heapq.merge(
+            merge_sort(unsorted, start, midpoint),
+            merge_sort(unsorted, midpoint, end))
